be74a6f3326722d86351d6bc26aa6df16f3feee5
Balazs O <balazs.otakomaiya@e3creative.co.uk>
2019-11-15 17:21:47 +0000

    Installed bitbucket bridge, removed redundant files/routes, migration

--- database/migrations/2019_11_15_170656_create_pull_requests_table.php
+++ database/migrations/2019_11_15_170656_create_pull_requests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePullRequestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('pull_requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('repo_slug');
+            $table->integer('pull_request_id');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('pull_requests');
     }
 }
